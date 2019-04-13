0e9f015165f2b3a85b09c121088f162d06a51385
calkinsr2017 <rcalkins@umd.edu>
2019-04-13 01:54:51 -0400

    person class

--- app/src/main/java/com/example/jesse/piecash/View/Person.java
+++ app/src/main/java/com/example/jesse/piecash/View/Person.java
@@ -1,11 +1,4 @@
-package main.java.com.example.jesse.piecash.View;
+package com.example.jesse.piecash.View;
 
 public class Person {
-     private String firstName;
-     private String lastName;
-     private int id;
-
-
-
-
 }
